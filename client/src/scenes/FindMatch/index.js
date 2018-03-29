b881a7bb22050dd4b3bde8415d4ba81f6c7afd62
Dylan Wagner <dwags618@gmail.com>
2018-03-29 07:38:59 -0500

    added initial google map functionality

--- client/src/scenes/FindMatch/index.js
+++ client/src/scenes/FindMatch/index.js
@@ -4,19 +4,41 @@ import Typography from 'material-ui/Typography';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { setTitle } from 'redux/navigation';
+import GoogleMapReact from 'google-map-react'
+const AnyReactComponent = ({ text }) => <div>{ text }</div>;
+
 
 class FindMatchPage extends Component {
   componentDidMount() {
     this.props.setTitle(this.props.translate('findmatch-page.title'));
   }
 
+ static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
   render() {
     const { translate } = this.props;
 
     return (
-      <Typography variant="title">
-        {translate('findmatch-page.title')}
-      </Typography>
+     // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyD9cAvlDLIsGj1EEmifL_NEiOS98IFs_Ak' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
+      </div>
     );
   }
 }
