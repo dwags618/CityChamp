f6ea16c629cd7df07568e019437da5c249d26703
dwags618 <dwags618@gmail.com>
2018-04-25 03:14:07 -0500

    styled search button and maps image

--- client/src/scenes/FindMatch/index.js
+++ client/src/scenes/FindMatch/index.js
@@ -7,6 +7,7 @@ import { setTitle } from '../../redux/navigation';
 import GoogleMapReact from 'google-map-react'
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import { withStyles } from 'material-ui/styles';
+import {geolocated} from 'react-geolocated';
 
 const AnyReactComponent = ({ text }) => <div>{ text }</div>;
 
@@ -17,12 +18,27 @@ const styles = theme => ({
     borderColor: '#51c4e1',
     padding: 20, 
     width: 200,
-    height: 15
+    height: 15,
+    marginRight: 20
+  },
+  search: {
+    borderRadius: 25,
+    borderWidth: 2,
+    backgroundColor: '#1f5592',
+    width: 200,
+    height: 45,
+    color: 'white'
+  },
+  map: {
+    position:'relative', 
+    marginTop: 20, 
+    marginLeft: 250, 
+    width: 350, 
+    height: 350
   }
 })
 
 class FindMatchPage extends Component {
-  
 
  static defaultProps = {
     center: {
@@ -38,22 +54,22 @@ login = () => {
 
 
   render() {
-    const { translate, classes } = this.props;
+    const { translate, classes, coords } = this.props;
 
     return (
-    <div>
+    <div style={{position: 'absolute', height: '100%', width: '100%'}}>
     <input type="text" className={classes.zipcode} placeholder={"Zip Code"}>
     </input>
     <input type="text" className={classes.zipcode} placeholder={"Zip Code"}>
     </input>
-    <input type="button" onclick="this.codeAddress" id="inputButtonGeocode" style={{width:'150px'}} title="Search" value="Search">
+    <input type="button" onclick="this.codeAddress" className={classes.search} value="Search">
     </input>
-    <div style={{paddingLeft: '150px'}}>
+    <div className={classes.map}>
 
      <Map google={this.props.google}  initialCenter={{
             lat: 41.906,
             lng: -87.63
-          }} style={{ width: '350px', height: '350px'}} zoom={14}>
+          }} zoom={14}>
  
         <Marker onClick={this.onMarkerClick}
                 name={'Current location'} />
@@ -63,6 +79,8 @@ login = () => {
             </div>
         </InfoWindow>
       </Map>
+
+      
       </div>
       </div>
     );
@@ -84,7 +102,15 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
+const GeolocatedContainer = geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+});
+
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyD9cAvlDLIsGj1EEmifL_NEiOS98IFs_Ak'
-})(withStyles(styles)(FindMatchPage))
+}, mapDispatchToProps, mapStateToProps, GeolocatedContainer)(withStyles(styles)(FindMatchPage))
+
 
