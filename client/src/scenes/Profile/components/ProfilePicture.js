21a4a922a732f927c1923eb848698bb42a957654
dwags618 <dwags618@gmail.com>
2018-05-05 01:47:51 -0500

    added profile picture component

---
+++ client/src/scenes/Profile/components/ProfilePicture.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import { getTranslate, getActiveLanguage } from 'react-localize-redux';
+import Typography from 'material-ui/Typography';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import Paper from 'material-ui/Paper';
+import { withStyles } from 'material-ui/styles';
+import addimage from './blank-profile-picture.png';
+import $ from 'jquery';
+
+const styles = theme => ({
+  hideButton: {
+        display: 'none',
+  },
+  customfileupload: {
+    color: '#ffffff',
+    display: 'block',
+    position: 'relative',
+    width: 200,
+    height: 46,
+    border: 0,
+    margin: 6,
+    background: 'linear-gradient(90deg, #3394c1, #4ac3e2 20%)',
+    paddingTop: 15,
+    '&:before': {
+        content: '\' \'',
+        display: 'block',
+        position: 'absolute',
+        top: 7,
+        right: '-16px',
+        background: 'inherit',
+        zIndex: -1
+    },
+    '&:focus': {
+        outline: 0
+    }
+  },
+  formContainerLeft: {
+    height: 600,
+    width: 350
+  },
+})
+
+class ProfilePicture extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: '',
+      imagePreviewUrl: '',
+      value: ''
+    };
+  }
+
+  _handleImageChange(e) {
+    $('#imgPreview').show();
+    $('#addimage').hide();
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+        this.setState({
+            file: file,
+            imagePreviewUrl: reader.result
+        });
+    }
+
+    reader.readAsDataURL(file)
+
+  }
+
+  render() {
+    let {imagePreviewUrl} = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = (<div><img src={imagePreviewUrl} alt={imagePreviewUrl} width={200} height={200} style={{paddingTop:20}} /></div>);
+    } 
+
+    const { translate, classes } = this.props;
+
+    return (
+       <Paper elevation={4} className={classes.formContainerLeft}>
+          <center>
+            <div>
+              <div className="imgPreview" id="imgPreview">
+                {$imagePreview}
+              </div>
+              <img width="200" height="200"  style={{paddingTop:20}} alt={addimage} src={addimage} id="addimage" class="addimage"    />
+                <div>
+                  <label for="file-upload" className={classes.customfileupload}>
+                    Upload Image
+                  </label>
+                  <input id="file-upload" type="file" className={classes.hideButton}  onChange={(e)=>this._handleImageChange(e)} />
+                </div>
+              </div>
+          </center>
+        </Paper>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    translate: getTranslate(state.get('locale')),
+    currentLangugage: getActiveLanguage(state.get('locale')).code
+  };
+};
+
+export default withRouter(connect(
+  mapStateToProps
+)(withStyles(styles)(ProfilePicture)));
--- client/src/scenes/Profile/index.js
+++ client/src/scenes/Profile/index.js
@@ -7,8 +7,7 @@ import { setTitle } from '../../redux/navigation';
 import Paper from 'material-ui/Paper';
 import { withStyles } from 'material-ui/styles';
 import Grid from 'material-ui/Grid';
-import addimage from './blank-profile-picture.png';
-import $ from 'jquery';
+import ProfilePicture from './components/ProfilePicture';
 
 const styles = theme => ({
   pageContainer: {
@@ -16,10 +15,6 @@ const styles = theme => ({
     flexDirection: 'column',
     height: '100%',
     margin: '0 auto',
-  },
-  formContainerLeft: {
-    height: 600,
-    width: 350
   },
   formContainerRightTop: {
     height: 275,
@@ -33,33 +28,7 @@ const styles = theme => ({
   },
   contentContainer: {
     flex: '1 0 auto',
-  },
-  hideButton: {
-        display: 'none',
-  },
-  customfileupload: {
-    color: '#ffffff',
-    display: 'block',
-    position: 'relative',
-    width: 200,
-    height: 46,
-    border: 0,
-    margin: 6,
-    background: 'linear-gradient(90deg, #3394c1, #4ac3e2 20%)',
-    paddingTop: 15,
-    '&:before': {
-        content: '\' \'',
-        display: 'block',
-        position: 'absolute',
-        top: 7,
-        right: '-16px',
-        background: 'inherit',
-        zIndex: -1
-    },
-    '&:focus': {
-        outline: 0
-    }
-  },
+  }
 })
 
 class MapsPage extends Component {
@@ -86,25 +55,6 @@ class MapsPage extends Component {
     console.log('handle uploading-', this.state.file);
   }
 
-  _handleImageChange(e) {
-    $('#imgPreview').show();
-    $('#addimage').hide();
-    e.preventDefault();
-
-    let reader = new FileReader();
-    let file = e.target.files[0];
-
-    reader.onloadend = () => {
-        this.setState({
-            file: file,
-            imagePreviewUrl: reader.result
-        });
-    }
-
-    reader.readAsDataURL(file)
-
-  }
-
   handleChange(event) {
     this.setState({value: event.target.value});
   }
@@ -114,36 +64,17 @@ class MapsPage extends Component {
   }
 
   render() {
-    let {imagePreviewUrl} = this.state;
-    let $imagePreview = null;
-    if (imagePreviewUrl) {
-      $imagePreview = (<div><img src={imagePreviewUrl} alt={imagePreviewUrl} width={200} height={200} style={{paddingTop:20}} /></div>);
-    } 
+ const { translate, classes } = this.props;
+  
 
-    console.log(this.state.pictures)
-    const { translate, classes } = this.props;
+   
 
     return (
       <div className={classes.pageContainer}>
         <div className={classes.contentContainer}>
           <Grid container justify="center">
             <Grid item>
-              <center>
-                <Paper elevation={4} className={classes.formContainerLeft}>
-                  <div>
-                    <div className="imgPreview" id="imgPreview">
-                      {$imagePreview}
-                    </div>
-                    <img width="200" height="200"  style={{paddingTop:20}} alt={addimage} src={addimage} id="addimage" class="addimage"    />
-                    <div>
-                      <label for="file-upload" className={classes.customfileupload}>
-                        Upload Image
-                      </label>
-                      <input id="file-upload" type="file" className={classes.hideButton}  onChange={(e)=>this._handleImageChange(e)} />
-                    </div>
-                  </div>
-                </Paper>
-              </center>
+              <ProfilePicture/>
             </Grid>
             <div style={{marginLeft: 50, marginTop:10}}>
             <Grid item>
