f60058f28b32f02807a07624ff09cd7c31d025a2
dwags618 <dwags618@gmail.com>
2018-05-11 05:12:46 -0500

    Geolocated works succefully, props are taken into MapForm

--- client/src/scenes/FindMatch/components/Geolocated.js
+++ client/src/scenes/FindMatch/components/Geolocated.js
@@ -1,47 +1,22 @@
 import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import { withStyles } from 'material-ui/styles';
 import {geolocated} from 'react-geolocated';
-
-const styles = theme => ({
-  map: {
-    position:'relative', 
-    marginTop: 20, 
-    marginLeft: 75, 
-    width: 750, 
-    height: 500
-  }
-})
+import MapForm from './MapForm';
 
 class Geolocated extends Component {
-
- static defaultProps = {
-    center: {
-      lat: 41.906,
-      lng: -87.63
-    },
-    zoom: 11
-  };
   
   render() {
-
     return (
-
      !this.props.isGeolocationAvailable
       ? <div>Your browser does not support Geolocation</div>
       : !this.props.isGeolocationEnabled
         ? <div>Geolocation is not enabled</div>
         : this.props.coords
-          ? <table>
-            <tbody>
-              <tr><td>latitude</td><td>{this.props.coords.latitude}</td></tr>
-              <tr><td>longitude</td><td>{this.props.coords.longitude}</td></tr>
-              <tr><td>altitude</td><td>{this.props.coords.altitude}</td></tr>
-              <tr><td>heading</td><td>{this.props.coords.heading}</td></tr>
-              <tr><td>speed</td><td>{this.props.coords.speed}</td></tr>
-            </tbody>
-          </table>
-          : <div>Getting the location data&hellip; </div>
+          ? <MapForm
+              google={this.props.google}
+              latitude={this.props.coords.latitude}
+              longitude={this.props.coords.longitude}
+              />
+          : <div>Getting the location data&hellip; </div> 
     );
   }
 }
