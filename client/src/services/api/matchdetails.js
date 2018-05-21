b6bddeb21ae2d7c6859dce4ed3b929930f981fd6
dwags618 <dwags618@gmail.com>
2018-05-20 23:52:00 -0500

    added matchdetails api

---
+++ client/src/services/api/matchdetails.js
@@ -0,0 +1,12 @@
+export const saveBetAmount = (params) => {
+  return fetch('/api/matchdetails', {
+    method: 'PUT',
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify(params)
+  });
+}
+
+
+
+
+
