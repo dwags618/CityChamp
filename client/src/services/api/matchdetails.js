0d524926ee8caefdda34d803605f405d86d1bf30
dwags618 <dwags618@gmail.com>
2018-05-21 16:55:11 -0500

    added getPlayers fetch request to retrieve user info for table

--- client/src/services/api/matchdetails.js
+++ client/src/services/api/matchdetails.js
@@ -1,8 +1,18 @@
+import Auth from '../../services/auth';
+
 export const saveBetAmount = (params) => {
   return fetch('/api/matchdetails', {
     method: 'PUT',
     headers: {'Content-Type':'application/json'},
     body: JSON.stringify(params)
+  });
+}
+
+export const getPlayers = () => {
+  return fetch('/api/matchdetails', {
+    headers: {
+      'Authorization': `bearer ${Auth.getToken()}`
+    },
   });
 }
 
