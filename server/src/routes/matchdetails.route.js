20d797093cba10b5399c253ebabdcab288403c42
dwags618 <dwags618@gmail.com>
2018-05-20 23:56:27 -0500

    code cleanup

--- server/src/routes/matchdetails.route.js
+++ server/src/routes/matchdetails.route.js
@@ -5,18 +5,6 @@ import models from '../models';
 
 export const bet_update = (req, res) => {
 
-  /*const userData = {
-        name: req.body.name,
-        username: req.body.username,
-        password: password,
-        minimumBet: req.body.rangeValue[0],
-        maximumBet: req.body.rangeValue[1]
-      };
-
-  models.User.create(userData)
-    .then(ok(res))
-    .catch(badRequest(res));*/
-
  models.User.update({minimumBet: req.body.rangeValue[0], maximumBet: req.body.rangeValue[1]}, {where: {username: req.body.username}})
  .then((sites) => {
   var response = sites;
@@ -26,9 +14,6 @@ export const bet_update = (req, res) => {
  .catch(badRequest(res));
     
 }
-
-    
-  
 
 const router = new Router();
 
