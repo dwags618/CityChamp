eadd45861de8ab705a3d4185ed68b7d40d4f3749
dwags618 <dwags618@gmail.com>
2018-05-23 03:46:20 -0500

    added saveImage api to store image in database

--- client/src/services/api/matchdetails.js
+++ client/src/services/api/matchdetails.js
@@ -8,6 +8,14 @@ export const saveBetAmount = (params) => {
   });
 }
 
+export const saveImage = (params) => {
+  return fetch('/api/matchdetails/image', {
+    method: 'PUT',
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify(params)
+  });
+}
+
 export const getPlayers = () => {
   return fetch('/api/matchdetails', {
     headers: {
